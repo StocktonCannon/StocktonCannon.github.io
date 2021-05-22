6576697a4b70705d04ca5def6a6d8bf4efacd9a0
StocktonCannon <can20034@byui.edu>
2021-05-22 13:37:59 -0600

    may 22

---
+++ lesson5/js/pancakes.js
@@ -0,0 +1,11 @@
+
+
+var currentDate = new Date();
+currentDate.toString;
+var b = currentDate.toString();
+var day = b.slice(0,3);
+
+if (day == "Fri"){
+    let pancakemsg = document.getElementById("pancakeswrapper");
+    pancakemsg.style.display = "block";
+}
