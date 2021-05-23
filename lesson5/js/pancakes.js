0b86bc447ed210920e32bb61091a29c71a480297
StocktonCannon <can20034@byui.edu>
2021-05-22 21:30:12 -0600

    added js date functionality

--- lesson5/js/pancakes.js
+++ lesson5/js/pancakes.js
@@ -5,6 +5,8 @@ currentDate.toString;
 var b = currentDate.toString();
 var day = b.slice(0,3);
 
+
+
 if (day == "Fri"){
     let pancakemsg = document.getElementById("pancakeswrapper");
     pancakemsg.style.display = "block";
